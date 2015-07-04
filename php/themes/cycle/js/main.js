aaf4572ef17e53d6d7305824baaf933241ec3f41
Corey Acri <acri.corey@gmail.com>
2015-07-04 19:45:40 -0400

    trying to get 2015 trip count on page again

--- php/themes/cycle/js/main.js
+++ php/themes/cycle/js/main.js
@@ -92,6 +92,18 @@ $(document).ready(function(){
       var users = snapshot.val();
       $('#totalRiders').html(users.count);
   });
+
+ google.setOnLoadCallback(function (){
+  var counterRef = new Firebase('https://cyclephilly.firebaseio.com/trips-count/');
+  counterRef.child('2015').on('value', function(snapshot) {
+      var counter2015 = snapshot.val();
+      $('#2015TotalTrips').html(counter2015);
+  }
+
+  console.log(counter2015 + "<-- this is the count for 2015 trips from firebaseio")
+
+
+
   var cycleRef = new Firebase('https://cyclephilly.firebaseio.com/trips-started/2015/');
 google.setOnLoadCallback(function (){
   cycleRef.on('value', function(snapshot) {
