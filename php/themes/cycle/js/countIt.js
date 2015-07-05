8d738f5d89dcf8533f0dd721e50e10f78e305711
Corey Acri <acri.corey@gmail.com>
2015-07-04 21:02:11 -0400

    beh

--- php/themes/cycle/js/countIt.js
+++ php/themes/cycle/js/countIt.js
@@ -1,8 +1,10 @@
+var counter2015;
+
 google.setOnLoadCallback(function() {
   $(function() {
    var counterRef = new Firebase('https://cyclephilly.firebaseio.com/trips-count/');
   counterRef.child('2015').on('value', function(snapshot) {
-      var counter2015 = snapshot.val();
+       counter2015 = snapshot.val();
       $('#2015TotalTrips').html(counter2015.total);
 });
 
